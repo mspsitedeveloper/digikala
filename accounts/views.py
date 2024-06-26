ee5d4a5e104d1fba2a0f84247ae125c9d2bf0594
mspsitedeveloper <ahmadmahdavi188@yahoo.com>
2024-06-26 16:38:49 +0330

    create costomuser

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-class SignUp(CreateView):
-    form_class = UserCreationForm
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
--- blog/models.py
+++ blog/models.py
@@ -4,19 +4,23 @@ from django.urls import reverse
 from django.utils import timezone
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    abstract = models.TextField()
-    body = models.TextField()
-    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
-    date = models.DateField(default=date.today())
-    photo = models.ImageField(upload_to='photo/%y/%m/%d')
-    tag = models.ForeignKey('blog.tag', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50 , verbose_name='عنوان')
+    abstract = models.TextField(verbose_name='خلاصه')
+    body = models.TextField(verbose_name='محتوا')
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE, verbose_name='نویسنده')
+    date = models.DateField(default=date.today(), verbose_name='تاریخ')
+    photo = models.ImageField(upload_to='photo/%y/%m/%d', verbose_name='تصویر')
+    tag = models.ForeignKey('blog.tag', on_delete=models.CASCADE, verbose_name='هشتگ')
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse("single", kwargs={'pk': self.pk })
+    
+    class Meta():
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
     
 class comment(models.Model):
     body = models.TextField(null=False, blank=False)
